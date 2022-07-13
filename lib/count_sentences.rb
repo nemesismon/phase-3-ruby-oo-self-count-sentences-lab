b3868eca9c0e30e158f0cb1f2e940d6913296f48
Michael <michael.w.kolb@gmail.com>
2022-07-13 13:19:03 -0400

    commit tree 26d431f4917247e1f3b1ff1d2699122a52a0790d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    temp_array = self.split(" ")
+    counter = 0
+    temp_array.each do |sentence|
+      if sentence.end_with?(".")
+        counter += 1
+      elsif sentence.end_with?("?")
+        counter += 1
+      elsif sentence.end_with?("!")
+        counter += 1
+      end
+    end
+    counter
   end
 end
